fb502ea11d1d70e92edaf48bd2228d9b777095f7
intello123 <93282495+intello123@users.noreply.github.com>
2021-11-09 14:18:17 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+let angle = 0
 servos.P0.setRange(10, 170)
 basic.forever(function () {
-	
+    for (let index = 0; index <= 16; index++) {
+        angle = index * 10 + 10
+        servos.P0.setAngle(angle)
+        basic.pause(100)
+    }
 })
