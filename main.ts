7d80007c2a08cc214f16c8cd2e599da0f936d778
intello123 <93282495+intello123@users.noreply.github.com>
2021-11-12 13:10:42 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-let index = 0
 let angle = 0
 servos.P0.setRange(10, 170)
 basic.forever(function () {
     for (let index = 0; index <= 16; index++) {
         angle = index * 10 + 10
         servos.P0.setAngle(angle)
-        basic.pause(100)
+        basic.pause(200)
     }
-    index = 0
-    for (let index = 0; index <= 32; index++) {
-        angle = index * -5 + 10
+    angle = 170
+    for (let index = 0; index <= 31; index++) {
+        angle = angle - 5
         servos.P0.setAngle(angle)
+        basic.pause(200)
     }
 })
